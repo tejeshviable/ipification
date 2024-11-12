9141db2fbb7f29fcbb07f71b6e77b9dc5e015a57
Tejeshvi Roy <tejeshvi@racloop.com>
2024-11-12 19:32:12 +0530

    removing cors

--- src/main/java/com/anios/ipification/controller/SmsController.java
+++ src/main/java/com/anios/ipification/controller/SmsController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin()
 @RequestMapping("/sms")
 public class SmsController {
 
