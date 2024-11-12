f3b23566db2f2ee0a414fc829890935a6ae531ff
Aakriti <aakriti@racloop.com>
2024-11-12 18:00:38 +0530

    CORS error fixing(1)

--- src/main/java/com/anios/ipification/controller/AuthController.java
+++ src/main/java/com/anios/ipification/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = " http://localhost:3000")
+@CrossOrigin()
 @RequestMapping("/2fa")
 public class AuthController {
 
