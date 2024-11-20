785247a1466d4908d5196bf7fd486bfcca2f055f
Aakriti <aakriti@racloop.com>
2024-11-20 23:47:10 +0530

    get redis data

--- src/main/java/com/anios/ipification/responseDTO/StatusResponseDTO.java
+++ src/main/java/com/anios/ipification/responseDTO/StatusResponseDTO.java
@@ -1,5 +1,6 @@
 package com.anios.ipification.responseDTO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StatusResponseDTO {
 
     private String txnId;
